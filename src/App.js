17328e3bb07457c028d64936b3efdf07d29f7c0f
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-11-06 15:35:56 +0530

    Added Implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import Fuse from 'fuse.js'
+import { Container } from 'react-bootstrap';
+import React, { Component } from 'react';
+import choice from './choices.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = {
+  // isCaseSensitive: false,
+  // includeScore: false,
+  // shouldSort: true,
+  // includeMatches: false,
+  // findAllMatches: false,
+  // minMatchCharLength: 1,
+  // location: 0,
+  // threshold: 0.6,
+  // distance: 100,
+  // useExtendedSearch: false,
+  // ignoreLocation: false,
+  // ignoreFieldNorm: false,
+  keys: [
+    "title",
+    "author.firstName"
+  ]
+};
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Container>
+            <input type="text" value={this.state.value} placeholder="Search" onChange={this.handleChange} style={{ width: "300px", height: "30px" }} />
+            <Search pattern={this.state.value} />
+          </Container>
+        </header>
+      </div>
+    );
+  }
 }
 
+function Search({ pattern }) {
+  const fuse = new Fuse(choice, options);
+  const t = fuse.search(pattern);
+  const result = t.map(character => <p>{character.item.title}</p>);
+  return <div>{result}</div>;
+}
 export default App;
